7ee0438add3f541a48d52a7777425d886fc23343
byte3ti <109740929+byte3ti@users.noreply.github.com>
2022-12-16 18:42:09 -0300

    Add files via upload
    primeiro commit

---
+++ js/animation.js
@@ -0,0 +1,44 @@
+const scrollRevealTop = ScrollReveal({
+  origin: 'top',
+  distance: '100px',
+  duration: 1000,
+  reset: true
+})
+
+scrollRevealTop.reveal(' .img2', { interval: 300 })
+
+//////////////////////////////////////////////////////
+
+const scrollRevealLeft = ScrollReveal({
+  origin: 'bottom',
+  distance: '100px',
+  duration: 1000,
+  reset: true
+})
+
+scrollRevealLeft.reveal('', { interval: 300 })
+
+const scrollRevealFixed = ScrollReveal({
+  origin: 0,
+  distance: 0,
+  duration: 1000,
+  reset: true,
+  delay: 300
+})
+
+scrollRevealFixed.reveal(
+  '.text1, .text2, .impHor, .impCap, .text3, .section1, .home',
+  {
+    interval: 300
+  }
+)
+
+const scrollRevealScale = ScrollReveal({
+  origin: 0,
+  distance: 0,
+  duration: 1000,
+  reset: true,
+  scale: 0
+})
+
+scrollRevealScale.reveal('.img1, video', { interval: 300 })
